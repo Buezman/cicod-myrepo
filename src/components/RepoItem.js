2028fbac7365b2c1d1520c75e34c4a6a0457d6a4
Buezman <54446003+Buezman@users.noreply.github.com>
2022-11-23 15:19:14 +0100

    fix: fix typo

--- src/components/RepoItem.js
+++ src/components/RepoItem.js
@@ -5,13 +5,14 @@ const RepoItem = ({ repo }) => {
     const date = new Date(repo.updated_at).getTime();
     const diff = today - date;
     const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    const val = days > 1 ? "days" : "day";
     return (
         <div className="card">
             <h3 className="repo_name">
                 <a href={repo.html_url}>{repo.name}</a>
             </h3>
             <p className="repo_language">
-                {repo.language} <span>{`Updated ${days} days ago`}</span>
+                {repo.language} <span>{`Updated ${days} ${val} ago`}</span>
             </p>
         </div>
     );
